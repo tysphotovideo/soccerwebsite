83ab9200ffc64bfa2b3c3fc67243b3ed5f01dc6e
tysphotovideo <tysphotovideo@gmail.com>
2023-05-26 15:30:57 -0500

    lets check if this works
    hey

--- djangoPy3Env/bin/activate.fish
+++ djangoPy3Env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/tylersellers/Documents/tysc/djangoPy3Env"
+set -gx VIRTUAL_ENV "/Users/tylersellers/Documents/Python/tysphotovideo.github.io/djangoPy3Env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
